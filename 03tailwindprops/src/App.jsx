09bd5d049d93f34740c3e5ca945a02834bd54626
AnakinCodeWalker <parag31ja@gmail.com>
2025-09-24 19:21:07 +0530

    learn about tailwind-components etc

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+/*
+https://youtu.be/qkbijl5EoHc?si=yTF7q0Fxcz2vThF0 
+installation guide  of tailwind css
+*/
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-green-400 m-4'>
+        
+          setting up the tailwind css
+      
+      </h1>
+    </>
+  )
+}
+
+export default App
---
+++ 03tailwindprops/src/components/Details.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from './card'
+export const Details = () => {
+  return (
+  <>
+  <div>Details</div>
+    <Card/>
+    </>
+    
+  )
+}
+
+export default Details
